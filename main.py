a05b3736d5d2ce54ba89a268f29d52cf8ff0752c
jsys12 <soloveuseva4@gmail.com>
2025-09-22 00:39:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ origins = [
     "http://127.0.0.1:5500",  # если запускаешь HTML через Live Server / локальный порт
     "http://localhost:5500",
     "http://127.0.0.1:8000",
-    "http://0.0.0.0:8000/"
+    "http://0.0.0.0:8000/",
+    "https://jsys12.github.io/SCAM.NET-site/"
 ]
 
 app.add_middleware(
